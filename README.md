c3c8c9a0fab2cf8ef9640f89041be3466f1bafa3
jbrucker <jebrucker@gmail.com>
2019-10-28 12:45:02 +0700

    initial code

---
+++ README.md
@@ -0,0 +1,14 @@
+## UML Practice
+
+Please contribute a UML practice problem.
+Problem can be either:
+* you provide code, others draw the UML
+* you provide UML, others write (partial) code based on UML
+
+Please create a descriptive subdirectory name without spaces,
+and put your problem in the directory, for example:
+
+```
+polygon-rectangle/    # Quiz 3 probme
+    polygons.py       # contains 3 classes for UML drawing
+```
---
+++ polygon-rectangle/polygons.py
@@ -0,0 +1,33 @@
+class Point:
+	"""Class represents a point in cartesian coordinates"""
+	def __init__(self, x:float, y:float):
+		self.x = x
+		self.y = y
+
+class Polygon:
+	def __init__(self, verticies): # verticies is a list of Points
+		self.points = verticies   
+
+	def area(self) -> float:
+		"""compute area of polygon"""
+		# code omitted
+		return 0
+
+	def contains(self, point: Point) -> bool:
+		"""test if Polygon contains a given point"""
+		# code omitted
+		return False
+
+class Rectangle(Polygon):
+	def __init__(self, x, y, width, height):
+		self.width, self.height = width, height
+		p1 = Point(x,y)
+		p2 = Point(x+width,y)
+		p3 = Point(x+width,y+height)
+		p4 = Point(x,y+height)
+		super().__init__([p1, p2, p3, p4])
+
+	# more efficient computation of area than done in Polygon
+	def area(self) -> float:
+		return abs( self.width * self.height )
+	
