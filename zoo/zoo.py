07fbe5efd4ae80a698bf96ea94f97c45dc322329
Thun Thunkijjanukij <thunthunkijjanukij@Thuns-MacBook-Pro.local>
2019-10-28 15:25:35 +0700

    add year check old

--- zoo/zoo.py
+++ zoo/zoo.py
@@ -2,10 +2,10 @@ import datetime
 
 class Animal:
     """Class of some animal"""
-    def __init__(self , name: str, sex:str,date):
+    def __init__(self , name: str, sex:str, year: int):
         self.name = name
         self.sex = sex
-        self.old = datetime.date - date
+        self.old = 2019-year
         
     
 class Zoo:
@@ -14,20 +14,20 @@ class Zoo:
         self.name = name
         self.all_cage = []
 
-    def add_animal(self,):
+    def add_animal(self,Cage: Cage):
         """Medthod for add animal_cage to cage"""
-        return 0
+        self.all_cage.append(Cage)
 
 class Tiger(Animal):
-    def __init__(self, name, sex, date):
-        super().__init__(name, sex, date)
+    def __init__(self, name: str, sex : str, year:int):
+        super().__init__(name, sex, year)
 
     def description(self) -> str:
-        print(f"{self.name} sex:{self.sex} date:{self.old}")
+        print(f"Tiger name:{self.name} sex:{self.sex} date:{self.old} ")
 
 
-class Cage():
-    def __init__(self,Animal):
+class Cage:
+    def __init__(self,Animal : Animal):
         self.cage = [Animal]
 
     def add_tiger(self,Animal:Animal):
