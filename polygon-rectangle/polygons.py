c70decf7fbc859f39e8a9c85917568a1a79776e2
jbrucker <jebrucker@gmail.com>
2019-10-29 13:14:26 +0700

    minor update for style

--- polygon-rectangle/polygons.py
+++ polygon-rectangle/polygons.py
@@ -6,35 +6,37 @@
 # Use correct UML notation, show multiplicity where it makes sense.
 #
 class Point:
-	"""Class represents a point in cartesian coordinates"""
-	def __init__(self, x:float, y:float):
-		self.x = x
-		self.y = y
+    """Class represents a point in cartesian coordinates"""
+    def __init__(self, x:float, y:float):
+        self.x = x
+        self.y = y
 
 class Polygon:
-	def __init__(self, verticies): # verticies is a list of Points
-		self.points = verticies   
+    def __init__(self, verticies): 
+        """Initialize a polygon with ordered list of verticies."""
+        self.points = verticies   
 
-	def area(self) -> float:
-		"""compute area of polygon"""
-		# code omitted
-		return 0
+    def area(self) -> float:
+        """compute the area of polygon"""
+        # code omitted
+        return 0
 
-	def contains(self, point: Point) -> bool:
-		"""test if Polygon contains a given point"""
-		# code omitted
-		return False
+    def contains(self, point: Point) -> bool:
+        """test if Polygon contains a given point"""
+        # code omitted
+        return False
 
 class Rectangle(Polygon):
-	def __init__(self, x, y, width, height):
-		self.width, self.height = width, height
-		p1 = Point(x,y)
-		p2 = Point(x+width,y)
-		p3 = Point(x+width,y+height)
-		p4 = Point(x,y+height)
-		super().__init__([p1, p2, p3, p4])
+    def __init__(self, x, y, width, height):
+        self.width, self.height = width, height
+        p_1 = Point(x, y)
+        p_2 = Point(x+width, y)
+        p_3 = Point(x+width, y+height)
+        p_4 = Point(x, y+height)
+        super().__init__([p_1, p_2, p_3, p_4])
 
-	# more efficient computation of area than done in Polygon
-	def area(self) -> float:
-		return abs( self.width * self.height )
-	
+    # more efficient computation of area than done in Polygon
+    def area(self) -> float:
+        """Return the area of this rectangle"""
+        return abs(self.width * self.height)
+    
