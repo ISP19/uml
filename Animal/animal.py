eee7ecc62b607066877792664f7c055ee1a466a1
Vichyawat Nakarugsa <vichyawat.n@ku.th>
2019-10-30 17:20:26 +0700

    fix constant

--- Animal/animal.py
+++ Animal/animal.py
@@ -6,40 +6,40 @@
 # Use correct UML notation, show multiplicity where it makes sense.
 #
 class Animal:
-	def __init__(self, name: string, legs: int):
-		self.name = name
-		self.legs = legs
-	
-	def change_name(self, name):
-		self.name = name
-	
-	def die(self):
-		"""Delete this object"""
-		# code omitted
-		pass
+    def __init__(self, name: str, legs: int):
+        self.name = name
+        self.legs = legs
+    
+    def change_name(self, name):
+        self.name = name
+    
+    def die(self):
+        """Delete this object"""
+        # code omitted
+        pass
 
 
 class Turtle(Animal):
-    def __init__(self, name:string, legs:int, species:string):
+    def __init__(self, name: str, legs: int, species: str):
         super().__init__(name, legs)
         self.species = species
-	
-	def eat(self, object: Seaweed, amount: int):
-		""" Decrease seaweed amount or delete seaweed object"""
-		# code omitted
-		pass
+
+    def eat(self, object: object, amount: int):
+        """ Decrease seaweed amount or delete seaweed object"""
+        # code omitted
+        pass
 
 
 class Seaweed():
-	def __init__(self, amount: int):
-		self.amount = amount
+    def __init__(self, amount: int):
+        self.amount = amount
 
 
 class Human():
-	def __init__(self, name: string):
-		self.name = name
-		
-	def feed(self, animal: Animal):
-		"""One human can feed food from 0 to many animals"""
-		# code omitted
-		pass
+    def __init__(self, name: str):
+        self.name = name
+        
+    def feed(self, animal: Animal):
+        """One human can feed food from 0 to many animals"""
+        # code omitted
+        pass
