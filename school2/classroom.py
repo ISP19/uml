ca47162e2cd2847b7c2fb45501eeafcdeeb76422
gunnkrub <gunnkrub2543@gmail.com>
2019-10-31 22:39:56 +0700

    Modified code using pylint

--- school2/classroom.py
+++ school2/classroom.py
@@ -1,26 +1,39 @@
+"""
+This is classroom
+"""
 class User:
-    def __init__(self, username:str, password:str, email:str, ID:int, course_list:list):
+    """
+    User class
+    """
+    def __init__(self, username: str, password: str, ID: int, course_list: list):
         self.username = username
         self.password = password
-        self.email = email
         self.ID = ID
         self.course_list = course_list
     def authenticate(self) -> bool:
         """return True or False"""
 
 class Teacher(User):
-    def __init__(self, user:User):
-        self.user = user
+    """
+    Teacher user class
+    """
+    def __init__(self, user: User):
+        super().__init__(user.username, user.password, user.ID, user.course_list)
     def create_course(self, course_name):
         """create course using course_name"""
 
 class Student(User):
-    def __init__(self, user:User):
-        self.user = user
+    """
+    Student user class
+    """
+    def __init__(self, user: User):
+        super().__init__(user.username, user.password, user.ID, user.course_list)
     def choose_course(self, course_name):
         """choose course from teacher"""
 
 class Course:
-    def __init__(self):
-        self.name = name
-    
+    """
+    Teacher and student course
+    """
+    def __init__(self, course_name):
+        self.name = course_name
