99e5a20efc7422901221a44fe90ce4cb6fc9bb64
kabilza <naruoluvohina@gmail.com>
2019-11-01 14:14:46 +0700

    Fixed using pylint

--- Airplane/airplane.py
+++ Airplane/airplane.py
@@ -1,36 +1,40 @@
-class Passengers:
-    #Contains info on individuals passengers such as names and age"
-    def __init__(self, name: str, age: int):
-        self.name = name
-        self.age = age
-    
-    def __str__(self):
-        return f"{self.name}"
-
-class Captain:
-    #Contains info on indivduals captains such as names and age"
+"""Airplane class and it's relationship between Captain and Passenger class"""
+class Passengers():
+    """Contains info on individuals passengers such as names and age"""
     def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
 
     def __str__(self):
+        """Return string"""
         return f"{self.name}"
 
-class Airplane:
-    #Contains name, models, capacity, captain(s), and list of passengers)
-    def __init__(self, name: str, model: str, capacity: int = 0
-                ,captain: Captain = ""):
+class Captain():
+    """Contains info on indivduals captains such as names and age"""
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        """Return string"""
+        return f"{self.name}"
+
+class Airplane():
+    """Contains name, models, capacity, captain(s), and list of passengers)"""
+    def __init__(self, name: str, model: str, capacity: int = 0, captain: Captain = ""):
         self.name = name
         self.model = model
         self.capacity = capacity
         self.captain = captain
         self.passengers = []
-        
+
     def __str__(self):
         return f"{self.name}"
 
     def add_passengers(self, passenger: Passengers):
+        """Add Passengers to the Airplane"""
         self.passengers.append(passenger)
 
     def add_captain(self, captain: Captain):
+        """Assign Captain to the Airplane"""
         self.captain = captain
