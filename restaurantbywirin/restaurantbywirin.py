6b22e77797c16bb8c04060889f31e26f6a75a6ba
Jirawadee Sampusri <jirawadeelovely@hotmail.com>
2019-11-04 00:26:11 +0700

    pylint

--- restaurantbywirin/restaurantbywirin.py
+++ restaurantbywirin/restaurantbywirin.py
@@ -1,51 +1,57 @@
+""" Module represents a calculation of foods in a restaurant. """
+
+
 class Restaurant:
-	"""Class represents a point in cartesian coordinates"""
-	def __init__(self,food:str,beverage:str,dessert:str, price:int,amount:int):
-		self.food = food
-		self.beverage = beverage
-		self.dessert = dessert
-		self.price = price
-		self.amount = amount
+    """ Class represents a point in cartesian coordinates. """
+    def __init__(self, food: str, beverage: str, dessert: str, price: int, amount: int):
+        self.food = food
+        self.beverage = beverage
+        self.dessert = dessert
+        self.price = price
+        self.amount = amount
+
 
 class Price:
-	def __init__(self, price): 
-		self.price = price
+    """ Class represents a price and promotion ."""
+    def __init__(self, price):
+        self.price = price
 
-	def sale(self) -> int:
-		"""promotion of food,beverage,dessert"""
-		return 50
+    def sale(self) -> int:
+        """ promotion of food, beverage, dessert. """
+        return 50
+
 
 class Food(Price):
+    """ Class represents a price and promotion of each food. """
+    def __init__(self, price: int, amount):
+        friedrice = Price(price)
+        noodles = Price(price)
+        self.amount = amount
+        super().__init__([friedrice, noodles])
 
-	def __init__(self, price:int,amount)
-		friedrice = Price(price)
-		noodles = Price(price)
-		self.amount = amount
-		super().__init__([friedrice,noodles])
+    def sale(self) -> int:
+        return(self.price*self.amount) - Price.sale
 
-	def sale(self) -> int:
-		return  (self.price * self.amount ) - Price.sale
 
 class Beverage(Price):
-	def __init__(self,beverage,price:int,amount):
-		juice = Price(price)
-		water = Price(price)
-		self.amount = amount
-		super().__init__([juice,water])
+    """ Class represents a price and promotion of each beverage. """
+    def __init__(self, price: int, amount):
+        juice = Price(price)
+        water = Price(price)
+        self.amount = amount
+        super().__init__([juice, water])
 
-	def sale(self) -> int:
-		return  (self.price * self.amount ) - Price.sale
+    def sale(self) -> int:
+        return(self.price * self.amount) - Price.sale
+
 
 class Dessert(Price):
-	def __init__(self,dessert,price:int,amount):
-		cake = Price(price)
-		bread = Price(price)
-		self.amount = amount
-		super().__init__([cake,bread])
+    """ Class represents a price and promotion of each dessert. """
+    def __init__(self, price: int, amount):
+        cake = Price(price)
+        bread = Price(price)
+        self.amount = amount
+        super().__init__([cake, bread])
 
-	def sale(self) -> int:
-		return  (self.price * self.amount ) - Price.sale
-
-
-	
-	
+    def sale(self) -> int:
+        return(self.price * self.amount)-Price.sale
