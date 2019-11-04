3729a3929a096ff9c22b7e58493068462bf2f5e1
Tiranan <tiranan.e@ku.th>
2019-11-04 17:36:10 +0700

    use pylint to edit file

--- Showroom/ShowroomCar.py
+++ Showroom/ShowroomCar.py
@@ -1,34 +1,44 @@
+class Price:
+    """This class show a price and promotion."""
+    def __init__(self, price):
+        self.price = price
+
+    def promotion_car(self) -> int:
+        """special promotion of car's price for special customer and member in company"""
+        return self.price*0.4
+
+    def promotion_motorbike(self) -> int:
+        """special promotion of motorbike's price for special customer and member in company"""
+        return self.price*0.5
+
+class Car(Price):
+    """ This class represents a price and promotion of each car. """
+    def __init__(self, price: int, amount):
+        honda = Price(price)
+        toyota = Price(price)
+        self.amount = amount
+        super().__init__([honda, toyota])
+
+    def promotion(self) -> int:
+        """special promotion for special customer and member in company"""
+        return (self.price*self.amount) - Price.promotion_car
+
+class Motorbike(Price):
+    """ This class represents a price and promotion of each motorbike. """
+    def __init__(self, price: int, amount):
+        kawazaki = Price(price)
+        yamaha = Price(price)
+        self.amount = amount
+        super().__init__([kawazaki, yamaha])
+
+    def promotion(self) -> int:
+        """special promotion for special customer and member in company"""
+        return (self.price*self.amount) - Price.promotion_motorbike
+
 class Showroom:
-    def __init__(self, car:str, motorbike:str, price:int, speed_max:int):
+    """This class represents a type and price of each car."""
+    def __init__(self, car: str, motorbike: str, price: int, speed_max: int):
         self.car = car
         self.motorbike = motorbike
         self.price = price
         self.speed_max = speed_max
-
-class Price:
-	def __init__(self, price): 
-		self.price = price
-
-	def promotion(self) -> int:
-		"""special promotion for special customer and member in company"""
-		return (self.price)*0.4
-
-class Car(Price):
-
-	def __init__(self, price:int,amount):
-		self.honda = Price(price)
-		self.toyota = Price(price)
-		self.amount = amount
-		super().__init__([honda,toyota])
-
-	def promotion(self) -> int:
-		return  (self.price * self.amount ) - Price.promotion
-
-class Motorbike(Price):
-
-    def __init__(self, price:int, amount):
-        self.kawazaki = Price(price)
-        self.yamaha = Price(price)
-        self.amount = amount
-		super().__init__([kawazaki,yamaha])
-        
